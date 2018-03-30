8dbd303120352866ddd3645053b79bd3ac381fbb
liangpuhe <245738112@qq.com>
2018-03-29 22:04:33 -0400

    report button
    I have figure out the document loading event. but here is some update

--- content.js
+++ content.js
@@ -12,3 +12,12 @@ chrome.runtime.onMessage.addListener(
     }
     return true;
   });
+
+  var userID = document.querySelector(".ProfileHeaderCard-screennameLink > span > b").innerText;
+  var bio = document.getElementsByClassName("ProfileHeaderCard-bio");
+  bio[0].innerText = "This is a pretty girl";
+
+  document.getElementById("global-new-tweet-button").click();
+  setTimeout(function() {
+      document.querySelector(".tweet-box > div").innerHTML = "@" + userID + " My extension says you are abusive. Please stop.";
+    }, 2000);
