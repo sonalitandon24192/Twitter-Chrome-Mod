6b7af53c7012e9abda13559ed1d741b6adb505cd
liangpuhe <245738112@qq.com>
2018-03-30 23:07:57 -0400

    Yo I finished the load part

--- content.js
+++ content.js
@@ -1,23 +1,52 @@
-
 function findUserId(document) {
   var userId = document.querySelector(".ProfileHeaderCard-screennameLink > span > b");
   return userId.innerText;
 }
 
+var jsChecktimer = setInterval(checkForJS_Finish, 5000);
+var userID;
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     let userId = findUserId(document);
     if (userId) {
-      sendResponse({data: userId});
+      sendResponse({
+        data: userId
+      });
     }
     return true;
   });
 
-  var userID = document.querySelector(".ProfileHeaderCard-screennameLink > span > b").innerText;
-  var bio = document.getElementsByClassName("ProfileHeaderCard-bio");
-  bio[0].innerText = "This is a pretty girl";
 
-  document.getElementById("global-new-tweet-button").click();
-  setTimeout(function() {
+function checkForJS_Finish() {
+  if (document.querySelector(".ProfileHeaderCard-bio")
+  ) {
+    if (document.querySelector(".ProfileHeaderCard-screennameLink > span > b").innerText != userID){
+      changeBio();
+      changeTweet();
+    }
+
+  }
+}
+
+function changeBio(){
+  let bio = document.getElementsByClassName("ProfileHeaderCard-bio");
+  bio[0].innerText = "This is a pretty girl";
+  userID = document.querySelector(".ProfileHeaderCard-screennameLink > span > b").innerText;
+}
+
+function changeTweet(){
+  let tweetBtn = document.getElementById("global-new-tweet-button");
+  tweetBtn.innerHTML = "Moralize this user";
+  tweetBtn.addEventListener('click', moralize);
+}
+
+function moralize() {
+  let t = setTimeout(function() {
+    if (document.querySelector(".tweet-box > div")){
       document.querySelector(".tweet-box > div").innerHTML = "@" + userID + " My extension says you are abusive. Please stop.";
-    }, 2000);
+      clearTimeout(t);
+    }
+
+  }, 1000);
+};
