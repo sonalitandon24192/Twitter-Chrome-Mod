81c260d7797ea2e1a2429d7c5f6670545f567a29
jberman5 <jberman5@terpmail.umd.edu>
2018-04-07 13:36:53 -0400

    updated Follow button

--- content.js
+++ content.js
@@ -24,6 +24,7 @@ function checkForJS_Finish() {
     if (document.querySelector(".ProfileHeaderCard-screennameLink > span > b").innerText != userID){
       changeBio();
       changeTweet();
+      changeToReport();
     }
 
   }
@@ -35,6 +36,18 @@ function changeBio(){
   userID = document.querySelector(".ProfileHeaderCard-screennameLink > span > b").innerText;
 }
 
+function changeToReport() {
+  console.log("testing");
+  let followBtn = document.getElementsByClassName("EdgeButton EdgeButton--secondary EdgeButton--medium button-text follow-text");
+  let followingBtn = document.getElementsByClassName("EdgeButton EdgeButton--primary EdgeButton--medium button-text following-text");
+  let unfollowBtn = document.getElementsByClassName("EdgeButton EdgeButton--danger EdgeButton--medium button-text unfollow-text");
+  followBtn[0].style.display="none";
+  //followingBtn[0].style.display="none";
+  //unfollowBtn[0].style.display="inline";
+  followBtn[0].innerText = "Report";
+  followBtn[0].classList.add("report-text");
+}
+// Note: Currently, these run everyewhere, in timeline and on profile page
 function changeTweet(){
   let tweetBtn = document.getElementById("global-new-tweet-button");
   tweetBtn.innerHTML = "Moralize this user";
