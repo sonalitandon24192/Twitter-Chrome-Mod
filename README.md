e7d9ecb284ce305bf4e50600f4af67c73eafe6b8
saebomk <saebom@umich.edu>
2018-06-03 15:21:55 -0400

    UI Modification
    - Applied Bootstrap
    - Removed moralizing
    - Added the number of words used (back-end should be updated, is it feasible?)

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Twitter-Chrome-Mod
+# Tweety-Holmes
+
+1. Download all files as .zip
+2. Unzip the file to a folder.
+3. Go to your Chrome browser and hit chrome://extensions in your URL bar.
+4. Click on "Load Unpacked" and select the downloaded folder.
+5. Visit Twitter.com and view abusive profile analyis information!
+6. Feel free to contribute to our code.
--- content.js
+++ content.js
@@ -1,5 +1,6 @@
-var jsChecktimer = setInterval(checkForJS_Finish, 500);
+var jsChecktimer = setInterval(checkForJS_Finish, 200);
 var userID;
+var item,abusive_list; // jSON returned from server. Making it public for highlighting abusive words on lazy loading
 
 function get_score(username, callback) {
     var url = "https://pumpkin-shortcake-65417.herokuapp.com/tpi?user="+username+"&numberTwit=200";
@@ -15,14 +16,24 @@ function get_score(username, callback) {
     request.send();
 }
 
+window.onscroll = function(ev) {
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      // you're at the bottom of the page
+      if(item.yes_no)
+        highlightAbusivePosts(abusive_list);
+  }
+};
+
 function checkabusive(data) {
-   var item= JSON.parse(data);
+  item= JSON.parse(data);
    console.log(item);
    if (item.yes_no == true) {
-      changeBio();
-      changeTweet();
+      abusive_list = item.word_list.map(function(d) { return d.word })
+      changeBio(abusive_list);
+      // changeTweet();
       changeAvi();
-      changeToReport();
+      // changeToReport();
+      highlightAbusivePosts(abusive_list);
    }
    else {
      console.log("this is a nice person");
@@ -33,9 +44,27 @@ function checkabusive(data) {
 
 
 function findUserId(document) {
-  let userId = document.querySelector(".ProfileHeaderCard-screennameLink > span > b");
-  return userId.innerText;
+  let userID = document.querySelector(".u-linkComplex-target");
+  return userID.innerText;
 }
+
+function highlightAbusivePosts(abusive_list){
+
+  var alltweets = document.querySelectorAll(".tweet-text");
+    for(i=0;i<alltweets.length;i++)
+    {
+      var tweet = alltweets[i].innerText.toLowerCase();
+      for(j=0;j<abusive_list.length;j++){
+        var reg = new RegExp("\\b" + abusive_list[j] + "\\b", 'i')
+        if(reg.test(tweet))
+        {
+          tweet =  tweet.replace(abusive_list[j], "<span><strong><u>" + abusive_list[j] +"</u></strong></span>");
+          alltweets[i].innerHTML = tweet;
+          alltweets[i].style.backgroundColor = "rgba(252, 66, 123,0.1)";
+        }
+      }
+    }
+  }
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
@@ -51,7 +80,7 @@ chrome.runtime.onMessage.addListener(
 function checkForJS_Finish() {
   if (document.querySelector(".ProfileHeaderCard-bio")
   ) {
-    if (document.querySelector(".ProfileHeaderCard-screennameLink > span > b").innerText != userID){
+    if (document.querySelector(".u-linkComplex-target").innerText != userID){
       //send get request
       userID = findUserId(document);
       get_score(userID, checkabusive);
@@ -63,33 +92,40 @@ function checkForJS_Finish() {
   }
 }
 
-function changeBio(){
+function changeBio(abusive_list){
   userID = document.querySelector(".ProfileHeaderCard-nameLink").innerText;
 
   var originalDiv = document.getElementsByClassName("ProfileHeaderCard-screenname");
+  var parents = document.getElementsByClassName("AppContent-main content-main u-cf");
+  parents[0].setAttribute("style", "margin-top:50px;");
 
   if (! document.getElementById("bio-box")) {
 
     // Parent Element
     var biobox = document.createElement("DIV");
-    originalDiv[0].appendChild(biobox);
+    originalDiv[0].insertAdjacentElement("afterend", biobox);
     biobox.id = "bio-box";
 
       // Title
       var biobox_title = document.createElement("DIV");
       biobox.appendChild(biobox_title);
-      biobox_title.id = "bio-box-title";
-      biobox_title.innerText = "Twitter Profile Identifier";
+      biobox_title.className = "panel panel-default";
+
+      // Title Body
+      var biobox_title_body = document.createElement("DIV");
+      biobox_title.appendChild(biobox_title_body);
+      biobox_title_body.className = "panel-body";
+      //biobox_title_body.innerText = "Tweety Holmes";
 
       // Title Image
       var logo = document.createElement("IMG");
-      logo.setAttribute("src", "chrome-extension://pldpfpfcdmnmdellocecpejmgpimfppj/icon.png");
+      logo.src = `chrome-extension://${chrome.runtime.id}/icon.png`;
       logo.setAttribute("id", "bio-box-img");
-      biobox.append(logo);
+      biobox_title_body.append(logo);
 
       // Box
       var charbox = document.createElement("DIV");
-      biobox.appendChild(charbox);
+      biobox_title_body.appendChild(charbox);
       charbox.id = "char-box";
 
         // Prompt Abusive_toggle
@@ -108,13 +144,50 @@ function changeBio(){
         var biobox_word = document.createElement("P");
         charbox.appendChild(biobox_word);
         biobox_word.id = "bio-box-text";
-        biobox_word.innerText = "Top 5 Abusive Words";
+        biobox_word.innerText = "Few abusive words used";
 
         // Abusive_words
-        var biobox_word_items = document.createElement("P");
-        biobox_word.appendChild(biobox_word_items);
-        biobox_word_items.id = "bio-box-highlight";
-        biobox_word_items.innerText = "Something";
+        var list_group = document.createElement("UL");
+        list_group.className = "list-group";
+        charbox.append(list_group);
+
+        if(abusive_list.length >= 7) {
+          for(i=0; i<7; i++) {
+            var biobox_word_items = document.createElement("LI");
+            list_group.appendChild(biobox_word_items);
+            biobox_word_items.className = "list-group-item d-flex justify-content-between align-items-center";
+            biobox_word_items.innerText = abusive_list[i];
+            var biobox_word_num = document.createElement("SPAN");
+            biobox_word_items.appendChild(biobox_word_num);
+            biobox_word_num.className ="badge badge-primary badge-pill";
+            biobox_word_num.innerText = "13";
+          }
+        }
+        else {
+          for(i=0; i<abusive_list.length; i++) {
+            var biobox_word_items = document.createElement("LI");
+            list_group.appendChild(biobox_word_items);
+            biobox_word_items.className = "list-group-item d-flex justify-content-between align-items-center";
+            biobox_word_items.innerText = abusive_list[i];
+            var biobox_word_num = document.createElement("SPAN");
+            biobox_word_items.appendChild(biobox_word_num);
+            biobox_word_num.className ="badge badge-primary badge-pill";
+            biobox_word_num.innerText = "13";
+          }
+        }
+
+        /*
+        var abusiveWordsToDisplay = "";
+        if(abusive_list.length >= 7) {
+          for(i=0; i<7; i++)
+            abusiveWordsToDisplay = abusiveWordsToDisplay + abusive_list[i] + " ";
+        }
+        else{
+          for(i=0; i<abusive_list.length; i++)
+            abusiveWordsToDisplay = abusiveWordsToDisplay + abusive_list[i] + " ";
+        }
+        biobox_word_items.innerText = abusiveWordsToDisplay;
+        */
 
     var bio1 = document.getElementsByClassName("ProfileHeaderCard-bio");
     var bio2 = document.getElementsByClassName("ProfileHeaderCard-location");
@@ -132,53 +205,12 @@ function changeBio(){
   }
 }
 
-function changeToReport() {
-  console.log("testing");
-  let followBtn = document.getElementsByClassName("EdgeButton EdgeButton--secondary EdgeButton--medium button-text follow-text");
-  let followingBtn = document.getElementsByClassName("EdgeButton EdgeButton--primary EdgeButton--medium button-text following-text");
-  let unfollowBtn = document.getElementsByClassName("EdgeButton EdgeButton--danger EdgeButton--medium button-text unfollow-text");
-  followBtn[0].style.display="none";
-  //followingBtn[0].style.display="none";
-  //unfollowBtn[0].style.display="inline";
-  followBtn[0].innerText = "Report";
-  followBtn[0].classList.add("report-text");
-}
-//Add image overlay to user profile picture
 function changeAvi() {
   let container = document.getElementsByClassName("ProfileAvatar-container")[0];      //Get parent of Profile Avatar
   let avi = document.getElementsByClassName("ProfileAvatar-image");                   //Get current avatar if you want to modify it at all
   var clone = document.createElement("img");                                          // Create image that will be the overlay
   clone.classList.add("ProfileAvatar-image");
-  clone.src = "chrome-extension://${chrome.runtime.id}/bad-mouth.png";                  //If you are using a local image remember to update the permissions in the manifest
+  clone.src = `chrome-extension://${chrome.runtime.id}/bad-mouth.png`;                  //If you are using a local image remember to update the permissions in the manifest
+  clone.style.opacity = "0.9";
   container.appendChild(clone);
 }
-// Note: Currently, these run everyewhere, in timeline and on profile page
-function changeTweet(){
-  let Btn = document.getElementsByClassName("NewTweetButton");
-  Btn[0].setAttribute("style", "background-color:#eb3b5a;");
-
-  let actionBtns = document.getElementsByClassName("ProfileMessagingActions-buttonWrapper");
-  if(actionBtns.length > 1){
-    for (let btn of actionBtns){
-      btn.classList.add("u-sizeFull");
-    }
-  }
-  let tweetBtn = document.getElementsByClassName("NewTweetButton-text");
-  tweetBtn[0].innerHTML = "Moralize this user";
-  tweetBtn[0].addEventListener('click', moralize);
-  let msgBtn = document.getElementsByClassName("DMButton-text");
-  if (msgBtn.length){
-    msgBtn[0].innerHTML = "Whisper to this user";
-  };
-
-}
-
-function moralize() {
-  let t = setTimeout(function() {
-    if (document.querySelector(".tweet-box > div")){
-      document.querySelector(".tweet-box > div").innerHTML = "@" + userID + " My extension says you are abusive. Please stop.";
-      clearTimeout(t);
-    }
-
-  }, 1000);
-};
